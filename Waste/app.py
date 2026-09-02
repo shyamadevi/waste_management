cd2a8f8b219e3fa6f11c03f890fa69ec595d8d37
Shyama Devi <shyamadevi407@gmail.com>
2026-09-02 22:29:36 +0530

    website

--- Waste/app.py
+++ Waste/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Waste Management System"
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+
+@app.route("/dump-locations")
+def dump_locations():
+    return render_template("dump_locations.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
